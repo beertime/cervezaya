e9a4ea650f214477e1f06b510437c169be0863a6
David Inga <davidsingal@icloud.com>
2015-04-26 12:19:32 +0200

    validate brand length

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,6 +1,8 @@
 class Brand < ActiveRecord::Base
 
   validates :name, presence: true
+  validates_length_of :color, :minimum => 7, :maximum => 7, :allow_blank => true
+
 
   mount_uploader :image, LogoUploader
   mount_uploader :alt_image, LogoUploader

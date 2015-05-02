7420a9d4bfff587c0262d4a33ac66adb46514516
David Inga <davidsingal@icloud.com>
2015-05-02 19:38:24 +0200

    added params to users

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,6 +4,7 @@ ActiveAdmin.register User do
   index do
     selectable_column
     id_column
+    column :username
     column :email
     column :current_sign_in_at
     column :sign_in_count
@@ -18,6 +19,7 @@ ActiveAdmin.register User do
 
   form do |f|
     f.inputs "Admin Details" do
+      f.input :username
       f.input :email
       f.input :password
       f.input :password_confirmation

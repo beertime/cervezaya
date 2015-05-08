fddaf9530235376c49b8b9dabd24320b6150f63d
David Inga <davidsingal@icloud.com>
2015-05-08 18:22:35 +0200

    puma server

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,21 @@ set :keep_releases, 5
 set :rbenv_ruby, '2.2.2'
 set :rails_env, 'production'
 
+# Puma
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_init_active_record, true
+set :puma_preload_app, true
+
 namespace :deploy do
 
   after :restart, :clear_cache do

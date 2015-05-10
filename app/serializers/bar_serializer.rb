24a7f57f2c2ca2212c6bff3fbed1e21a19cadefe
David Inga <davidsingal@icloud.com>
2015-05-10 21:09:59 +0200

    bar detail

--- app/serializers/bar_serializer.rb
+++ app/serializers/bar_serializer.rb
@@ -5,8 +5,6 @@ class BarSerializer < ActiveModel::Serializer
     :latitude, :longitude, :favorite, :product_image_small,
     :product_image_medium, :product_image_large, :product_name, :product_price
 
-  has_many :products
-
   def address
     "#{object.address.split(',')[0]}, #{object.address.split(',')[1]}"
   end
@@ -32,23 +30,23 @@ class BarSerializer < ActiveModel::Serializer
   end
 
   def product_image_small
-    "#{object.products.first.brand.image.small}"
+    # "#{object.products.joins(:brand).first.brand.image.small}"
   end
 
   def product_image_medium
-    "#{object.products.first.brand.image.medium}"
+    # "#{object.products.joins(:brand).first.brand.image.medium}"
   end
 
   def product_image_large
-    "#{object.products.first.brand.image.large}"
+    # "#{object.products.joins(:brand).first.brand.image.large}"
   end
 
   def product_name
-    object.products.first.brand.name
+    # object.products.joins(:brand).first.brand.name
   end
 
   def product_price
-    object.products.first.price
+    0
   end
 
 end

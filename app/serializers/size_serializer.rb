2c7ca141c5d8f0e45f7abbddda0cb6d0276683e0
David Inga <davidsingal@icloud.com>
2015-05-10 19:21:49 +0200

    removed cache

--- app/serializers/size_serializer.rb
+++ app/serializers/size_serializer.rb
@@ -1,4 +1,3 @@
 class SizeSerializer < ActiveModel::Serializer
-  cache key: 'sizes', expires_in: 1.day
   attributes :id, :icon, :volume
 end
--- app/serializers/type_serializer.rb
+++ app/serializers/type_serializer.rb
@@ -1,4 +1,3 @@
 class TypeSerializer < ActiveModel::Serializer
-  cache key: 'types', expires_in: 1.day
   attributes :id, :name
 end

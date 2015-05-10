73c421850657326c35a9f2809eab87cc0eb1dfca
David Inga <davidsingal@icloud.com>
2015-05-10 19:47:46 +0200

    bar products

--- app/serializers/bar_serializer.rb
+++ app/serializers/bar_serializer.rb
@@ -1,6 +1,6 @@
 class BarSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :photo, :address, :phone, :rank, :latitude, :longitude, :favorite
-
+  has_many :products
   def favorite
     false
   end

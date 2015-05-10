b7f12b795f059aba700c4152235c38733e323056
David Inga <davidsingal@icloud.com>
2015-05-10 21:44:25 +0200

    prmotions start_date && end_date

--- app/controllers/api/promotions_controller.rb
+++ app/controllers/api/promotions_controller.rb
@@ -3,13 +3,20 @@ class API::PromotionsController < ApplicationController
   respond_to :json
 
   def index
+    start_date = params[:start_date]
+    end_date = params[:end_date]
     date = params[:date]
     bar = params[:bar]
     promotions = Promotion.where(published: true)
-    unless date.blank?
+
+    if !date.blank?
       promotions = promotions.where('start_date <= ?', date)
-        .where('end_date >= ?', date || Time.now.to_date)
+        .where('end_date >= ?', date)
+    elsif !start_date.blank?
+      promotions = promotions.where('start_date <= ?', start_date)
+        .where('end_date >= ?', end_date || Time.now.to_date)
     end
+
     render json: promotions, status: 200
   end
 

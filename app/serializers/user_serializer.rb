ff90810d79d587d0a7fce14dafefd23a43fef537
David Inga <david.inga@icloud.com>
2015-05-17 18:38:13 +0200

    username added

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
 
-  attributes :id, :push_uid, :facebook_uid, :google_uid, :email, :publicname, :avatar, :gender, :birth, :city, :country
+  attributes :id, :push_uid, :facebook_uid, :google_uid, :username, :email, :publicname, :avatar, :gender, :birth, :city, :country
 
   def avatar
     object.avatar.small.try(:image)

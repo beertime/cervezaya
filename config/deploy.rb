75c0d4c3b88d0a0c764667a9d829cc1114c04a10
David Inga <david.inga@icloud.com>
2015-06-05 20:37:06 +0200

    change origin git

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'cervezaya'
-set :repo_url, 'git@github.com:socialscreen/cervezaya.git'
+set :repo_url, 'git@github.com:beertime/cervezaya.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp

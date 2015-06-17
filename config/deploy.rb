0db25fb4f62b3695afda6746a50433e54b1a6b8d
David Inga <david.inga@icloud.com>
2015-06-17 22:22:19 +0200

    added restart command

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,11 +43,12 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       within release_path do
-        execute :rake, 'cache:clear'
+        execute :rake, 'tmp:clear'
       end
       # Restart Puma
-      execute :sudo, 'restart puma app=#{release_path}'
+      execute :sudo, "restart puma app=#{current_path}"
     end
   end
 
+  after 'deploy:publishing', 'deploy:restart'
 end

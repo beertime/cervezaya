6fe5248889dd8ab26a2ba0209d11c0f703973247
David Inga <david.inga@icloud.com>
2015-07-04 20:55:03 +0200

    custom small url

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register User do
 
   permit_params :push_uid, :facebook_uid, :google_uid, :username,
-    :publicname, :email, :avatar, :country, :city, :gender, :birth, :active
+    :publicname, :email, :avatar, :country, :city, :gender, :birth, :active, :custom_avatar
 
   index do
     selectable_column

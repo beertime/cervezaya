5ee58b2fadce09f73f49053b6a192f8788a3e9ac
ialex90 <alejandrofpo@gmail.com>
2015-07-05 12:17:31 +0200

    Commit
    Implementing home, contact and press

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,22 +2,22 @@
 //= require jquery_ujs
 //= require bootstrap/dist/js/bootstrap
 
-// $(document).on('ready', onDocumentReady);
+$(document).on('ready', onDocumentReady);
 
-// function onDocumentReady() {
-//   $('.footer-social img').on('mouseover', function(){
-//     var photo = $(this).attr('data-hover');
-//     socialIconHover($(this), photo);
-//   })
+function onDocumentReady() {
+   $('.footer-social img').on('mouseover', function(){
+     var photo = $(this).attr('hover');
+     socialIconHover($(this), photo);
+   })
 
-//   $('.footer-social img').on('mouseout', function(){
-//     var photo = $(this).attr('data-normal');
-//     socialIconHover($(this), photo);
-//   })
-// }
+   $('.footer-social img').on('mouseout', function(){
+     var photo = $(this).attr('photo');
+     socialIconHover($(this), photo);
+   })
+ }
 
 
-// // Functions
-// var socialIconHover = function(img, photo){
-//   $(img).attr('src', 'assets/'+photo);
-// }
+//  Functions
+ var socialIconHover = function(img, photo){
+   $(img).attr('src', 'assets/'+photo);
+ }

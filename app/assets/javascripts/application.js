5e880cb78c8b3696e3983016ab54ceb7eb833860
David Inga <david.inga@icloud.com>
2015-07-05 12:51:55 +0200

    hover icons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,22 +2,6 @@
 //= require jquery_ujs
 //= require bootstrap/dist/js/bootstrap
 
+function onDocumentReady() {}
+
 $(document).on('ready', onDocumentReady);
-
-function onDocumentReady() {
-   $('.footer-social img').on('mouseover', function(){
-     var photo = $(this).attr('hover');
-     socialIconHover($(this), photo);
-   })
-
-   $('.footer-social img').on('mouseout', function(){
-     var photo = $(this).attr('photo');
-     socialIconHover($(this), photo);
-   })
- }
-
-
-//  Functions
- var socialIconHover = function(img, photo){
-   $(img).attr('src', 'assets/'+photo);
- }

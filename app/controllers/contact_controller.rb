d8e8330c902140a105d03cc1c93b97907ce72093
David Inga <david.inga@icloud.com>
2015-07-06 05:46:05 +0200

    contact mail

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,7 +1,7 @@
 class ContactController < ApplicationController
   def send_mail
     phone = params[:phone]
-    ContactMailer.business_mail(phone)
+    ContactMailer.business_mail(phone).deliver
     redirect_to empresas_y_bares_path, notice: 'Mensaje enviado correctamente'
   end
 end

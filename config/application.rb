2b14d9db0516e73d35f08d82651f5deabc1bdfa0
David Inga <david.inga@icloud.com>
2015-07-19 12:10:47 +0200

    disabling https

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Cervezaya
 
     # Forcing https
     # config.force_ssl = Rails.env != 'development'
+    config.force_ssl = false
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
